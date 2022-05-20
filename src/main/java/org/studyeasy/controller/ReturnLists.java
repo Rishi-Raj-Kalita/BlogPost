026fc0b6f94dc6a6bdd1699e322ca633808e9758
Rishiraj Kalita <rishirajkalita13@gmail.com>
2022-05-20 16:59:59 +0530

    add post, view post

--- src/main/java/org/studyeasy/controller/ReturnLists.java
+++ src/main/java/org/studyeasy/controller/ReturnLists.java
@@ -2,7 +2,10 @@ package org.studyeasy.controller;
 
 import java.util.List;
 
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.studyeasy.entity.*;
 import org.studyeasy.service.PostsService;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +16,18 @@ public class ReturnLists {
 	@RequestMapping("/lists")
 	public List<Post> list() {
 	    return new PostsService().getPosts(); 
+	}
+	
+	@RequestMapping("/lists/{id}")
+	public Post getPost(@PathVariable int id)//PathVariable(telling the name is used diff name)
+	{
+		return new PostsService().getPost(id);
+	}
+	
+	@RequestMapping(method=RequestMethod.POST, value="/lists")
+	public void addPost(@RequestBody Post post)
+	{
+		new PostsService().addPost(post);
 	}
 
 }
